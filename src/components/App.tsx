fc2732c837a71156ed597d5b4d72b5d38604027f
Manon Moutier <manon.moutier@hotmail.fr>
2024-10-21 21:27:02 +0200

    PokeCard composent creation

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import "../App.css"
+import PokeCard from "./PokemonCard"
+
+function App(){
+	return (
+	<div>
+	<PokeCard/>
+	</div>
+);
+}
+
+export default App;
---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,8 @@
+function PokeCard() { 
+    return <figure>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbassaur"/>
+        <figcaption>Bulbasaur</figcaption>
+    </figure>;
+}
+
+export default PokeCard;
