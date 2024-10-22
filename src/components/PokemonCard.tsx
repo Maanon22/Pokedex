c1d6f28f18bcc5b58e83b7d403eaa571430aed17
Manon Moutier <manon.moutier@hotmail.fr>
2024-10-22 17:04:08 +0200

    Quete3 composant dynamique

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,8 +1,22 @@
 function PokeCard() { 
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbassaur"/>
-        <figcaption>Bulbasaur</figcaption>
-    </figure>;
+    const pokemon=pokemonList[0];
+    return(
+        <figure>
+			{pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name}/>):(<p>???</p>)}
+			<figcaption>{pokemon.name}</figcaption>
+		</figure>
+        );
 }
+
+const pokemonList = [
+    {
+        name: "bulbasaur",
+		imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+        name: "mew",
+	},
+];
 
 export default PokeCard;
